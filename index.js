ab1943886a16959cb7e6fd46da3915bc2548ec83
+ <+>
2021-09-24 11:02:01 -0700

    commit tree 4389fc19092241b10524094072f0ed6c559d80e1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+// writeCards()
+// returns an array of thank you messages
+// for each name provided to the function:
+
+let names = ["Lisa", "Katlin", "Jan"];
+
+const writeCards = function (name){
+    console.log (`Thank you, ${name} ,for the wonderful surprise gift`);
+}
+
+// let writeCards = ["Lisa", "Katlin", "Jan"];
